d537e4099a18361aff4b75948489457bf6d0d489
eos-del <karthikeyathurlapati@gmail.com>
2026-07-27 22:43:12 +0530

    Update firebase.js

--- accounts/firebase.js
+++ accounts/firebase.js
@@ -1,4 +1,30 @@
-// EOS Firebase Configuration
-// This file will be connected to your Firebase project.
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
-console.log("EOS Firebase Ready");
+// YOUR FIREBASE CONFIG
+const firebaseConfig = {
+  apiKey: "AIzaSyBXeNMJe6wMEb2u5KfDwOURenwF8f7Nlvg",
+  authDomain: "eos-platform-of-gods.firebaseapp.com",
+  projectId: "eos-platform-of-gods",
+  storageBucket: "eos-platform-of-gods.firebasestorage.app",
+  messagingSenderId: "313477636204",
+  appId: "1:313477636204:web:4c869dd4e207dc01a03849"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Make functions available to index.html
+window.auth = auth;
+window.createUserWithEmailAndPassword = createUserWithEmailAndPassword;
+window.signInWithEmailAndPassword = signInWithEmailAndPassword;
+window.signOut = signOut;
+window.onAuthStateChanged = onAuthStateChanged;
